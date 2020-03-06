fc346a07209da1f321c914bdc977f8d6298a5859
Mosolygó Balázs <mosolygo.iskolai@gmail.com>
2020-03-06 15:13:13 +0100

    Compatibility testing

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setuptools.setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.4',
+    python_requires='>=3.5',
     entry_points={
         'console_scripts': [
             'gitlogparser=gitlogparser.main:main',
